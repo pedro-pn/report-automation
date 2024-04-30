08b9fafe45a2a937b3f1dbbf894cd6d8158d7ecb
Pedro Paulo <pe.pn03@gmail.com>
2024-04-30 11:09:02 -0300

    Implement RDO full name when creating report sheet

--- Código.js
+++ Código.js
@@ -1,35 +1,45 @@
-function onFormSubmit(e) {
-    var formObject = e.response
-    var reportObject = createReportSpreadSheet(formObject);
-
-
-    //teste
+const greportModelID = "1stkfMCGdpIDEc1u4xfS3Ldl9qXirgR_4mPYMcCjSxBM"
+const reportInfoID = "1CEXqNgVBJOohszlvzw3B10nchUQ2eUIk"
+function onFormSubmit(formData) {
+  var formObject = formData.response
+  var reportObject = createReportSpreadSheet(formObject);
 
 
 }
 
-// function searchColumn(formObject, columnName) {
-//   var data = formObject.getDataRange().getValues();
-//   for (var col = 0; col < data[0].length; col++) {
-//     if (data[0][col] === columnName) {
-//       // Column found, return its index
-//       return (col + 1); // Adding 1 to convert to 1-based index
-//     }
-//   }
-//   // Column not found
-//   return -1;
-// }
+function updateReportNumber(formObject, type) {
+  var reportFile = DriveApp.getFileById(reportInfoID);
+  var reportInfoData = reportFile.getBlob().getDataAsString();
+  let data = JSON.parse(reportInfoData);
+  const projectToUpdate = getProjectName(formObject);
+  const reportNumberToUpdate = data.Projects.find(project => project.Name === projectToUpdate);
 
-function searchColumnResponse(formResponse, columnName) {
-  var itemResponses = formResponse.getItemResponses();
+  if (reportNumberToUpdate) {
+    switch (type){
+      case (1):
+        reportNumberToUpdate.RDO += 1;
+    }
+    const updatedInfoData = JSON.stringify(data, null, 2);
+    // var updatedBlob = Utilities.newBlob(updatedInfoData, 'application/json');
+    reportFile.setContent(updatedInfoData);
+  }
+  return (reportNumberToUpdate.RDO);
+}
+
+function getRDONumber(formObject) {
+  return (updateReportNumber(formObject, 1));
+}
+
+function searchColumnResponse(formObject, columnName) {
+  var itemResponses = formObject.getItemResponses();
   
   // Log the form response details
   for (var i = 0; i < itemResponses.length; i++) {
     var itemResponse = itemResponses[i];
+    console.log('Question: ' + itemResponse.getItem().getTitle());
+    console.log('Response: ' + itemResponse.getResponse());
     if (itemResponse.getItem().getTitle() == columnName)
       return (itemResponse.getResponse());
-    Logger.log('Question: ' + itemResponse.getItem().getTitle());
-    Logger.log('Response: ' + itemResponse.getResponse());
   }
 }
 
@@ -37,23 +47,19 @@ function getProjectName(formObject) {
   return (searchColumnResponse(formObject, "Projeto"));
 }
 
-// function getProjectName(formObject) {
-//   var projectNameColumn = searchColumn(formObject, "Projeto");
-//   return (projectNameColumn.getLastRow().getValue());
-// }
+function getReportDate(formObject) {
+  var date = searchColumnResponse(formObject, "Data do relatório");
+  var dateComponents = date.split('-');
+  var day = dateComponents[2];
+  var month = dateComponents[1];
+  var year = dateComponents[0];
 
-function getReportDate(e) {
-
-
+  return (day + '-' + month + '-' + year);
 }
-function createReportSpreadSheet(e) {
-  // Replace 'SOURCE_SPREADSHEET_ID' with the ID of the spreadsheet you want to duplicate
-  var sourceSpreadsheet = SpreadsheetApp.openById('198axqsZqkKf78v08HBMyaJhloO3p2rqRoBJfsaYOui0');
-  // Duplicate the spreadsheet
+function createReportSpreadSheet(formObject) {
+  var sourceSpreadsheet = SpreadsheetApp.openById(greportModelID);
   var copiedSpreadsheet = DriveApp.getFileById(sourceSpreadsheet.getId()).makeCopy();
-  
-  // Rename the duplicated spreadsheet if needed
-  copiedSpreadsheet.setName('RDO - ' + getProjectName(e) + ' - 000')
+  copiedSpreadsheet.setName(getProjectName(formObject) + ' - RDO ' + getRDONumber(formObject) + ' - ' + getReportDate(formObject) + '')
 
   return (copiedSpreadsheet)
 }
