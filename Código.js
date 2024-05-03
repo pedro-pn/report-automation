906573d103bf0cc83c53bb3043df2ca50d1cccd1
Pedro Paulo <pe.pn03@gmail.com>
2024-05-03 17:56:47 -0300

    Add functions to fill report header: time, employees and work break

--- Código.js
+++ Código.js
@@ -4,56 +4,52 @@ const reportInfoID = "1CEXqNgVBJOohszlvzw3B10nchUQ2eUIk"
 const weekday = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
 
 class ReportInfo {
-  constructor() {
-    this.reportInfoFile = DriveApp.getFileById(reportInfoID);
-    this.reportInfoString = this.reportInfoFile.getBlob().getDataAsString();
-    this.reportInfoData = JSON.parse(this.reportInfoString);
-  }
+	constructor() {
+    	this.reportInfoFile = DriveApp.getFileById(reportInfoID);
+    	this.reportInfoString = this.reportInfoFile.getBlob().getDataAsString();
+    	this.reportInfoData = JSON.parse(this.reportInfoString);
+  	}
 
   // This function MUST be called after using reportInfo.json. 
-  updateReportInfo() {
-    const updatedInfoData = JSON.stringify(this.reportInfoData, null, 2);
-    this.reportInfoFile.setContent(updatedInfoData);
-  }
+  	updateReportInfo() {
+   		const updatedInfoData = JSON.stringify(this.reportInfoData, null, 2);
+  		this.reportInfoFile.setContent(updatedInfoData);
+	}
 
-  getProjectInfo(projectName) {
-    return (this.reportInfoData.Projects.find(project => project.Name === projectName));
-  }
+	getProjectInfo(projectName) {
+    	return (this.reportInfoData.Projects.find(project => project.Name === projectName));
+  	}
 
-  getClient(projectName) {
-      return (this.getProjectInfo(projectName).Client)
-  }
-
-  updateRDO(projectName) {
-    this.getProjectInfo(projectName).RDO += 1;
-  }
-
-  getCNPJ(projectName) {
-    return (this.getProjectInfo(projectName).CNPJ)
-  }
-
-  getRDO(projectName) {
-    return (this.getProjectInfo(projectName).RDO)
-  }
-
-  getProposal(projectName) {
-    return (this.getProjectInfo(projectName).Proposal)
-  }
+	updateRDO(projectName) {
+    	this.getProjectInfo(projectName).RDO += 1;
+  	}
 }
 
 class ReportData {
   constructor(formObject) {
     this.formObject = formObject;
     this.reportInfo = new ReportInfo();
-    this.name = this.getProjectName(formObject);
-    this.date = this.getReportDate(formObject);
-    this.rdo = this.getRDONumber(this.name) + 1;
+    this.name = this.getProjectName();
+    this.date = this.getReportDate();
+    this.rdo = this.getRDONumber() + 1;
     this.reportSpreadSheet;
     this.reportSpreadSheetFile;
   }
 
-  getRDONumber(reportName) {
-    return (this.reportInfo.getRDO(reportName));
+  getRDONumber() {
+    return (this.reportInfo.getProjectInfo(this.name).RDO);
+  }
+
+  getClient() {
+    return (this.reportInfo.getProjectInfo(this.name).Client);
+  }
+
+  getCNPJ() {
+    return (this.reportInfo.getProjectInfo(this.name).CNPJ);
+  }
+
+  getProposal() {
+    return (this.reportInfo.getProjectInfo(this.name).Proposal);
   }
 
   getWeekDay() {
@@ -63,19 +59,17 @@ class ReportData {
     return (weekday[weekDay]);
   }
   
-  searchColumnResponse(formObject, columnName) {
-      var itemResponses = formObject.getItemResponses();
+  searchFieldResponse(fieldName) {
+      var itemResponses = this.formObject.getItemResponses();
       for (var i = 0; i < itemResponses.length; i++) {
         var itemResponse = itemResponses[i];
-        console.log('Question: ' + itemResponse.getItem().getTitle());
-        console.log('Response: ' + itemResponse.getResponse());
-        if (itemResponse.getItem().getTitle() == columnName)
+        if (itemResponse.getItem().getTitle() == fieldName)
           return (itemResponse.getResponse());
       }
     }
 
-  getReportDate(formObject) {
-      var date = this.searchColumnResponse(formObject, "Data do relatório");
+  getReportDate() {
+      var date = this.searchFieldResponse("Data do relatório");
       var dateComponents = date.split('-');
       var day = dateComponents[2];
       var month = dateComponents[1];
@@ -84,8 +78,8 @@ class ReportData {
       return (day + '-' + month + '-' + year);
     }
     
-    getProjectName(formObject) {
-      return (this.searchColumnResponse(formObject, "Projeto"));
+    getProjectName() {
+      return (this.searchFieldResponse("Projeto"));
     }
 
     openReportSpreadSheet() {
@@ -93,12 +87,54 @@ class ReportData {
     }
 }
 
-function fillReportHeader(reportData) {
-  var reportSpreadSheet = reportData.reportSpreadSheet
+function	fillReportNightShift(reportData, reportFirstSheet) {
+	var nightShiftFlag = reportData.searchFieldResponse("Houve turno noturno?");
+
+	if (nightShiftFlag === "Não")
+		return ;
+	
+	var nightShiftStartTime = reportData.searchFieldResponse("Horário de inicio do turno noturno");
+	var nightShiftExitTime = reportData.searchFieldResponse("Horário de saída do turno noturno");
+	var nightShiftDinnerTime = reportData.searchFieldResponse("Tempo total de intervalo de janta");
+	var	nightShiftNumOfEmployees = reportData.searchFieldResponse("Quantidade de colaboradores no turno noturno");
+	console.log(nightShiftFlag);
+	console.log(nightShiftStartTime);
+	console.log(nightShiftExitTime);
+	console.log(nightShiftDinnerTime);
+	console.log(nightShiftNumOfEmployees);
+	reportFirstSheet.getRange("D7").setValue(nightShiftStartTime);
+	reportFirstSheet.getRange("D8").setValue(nightShiftExitTime);
+	reportFirstSheet.getRange("H8").setValue(nightShiftDinnerTime);
+	reportFirstSheet.getRange("L8").setValue(nightShiftNumOfEmployees);
+}
+
+function  fillReport(reportData) {
+  var reportSpreadSheet = reportData.reportSpreadSheet;
   var reportFirstSheet = reportSpreadSheet.getSheets()[0];
 
+  fillReportHeader(reportData, reportFirstSheet);
+  fillReportSubHeader(reportData, reportFirstSheet);
+  fillReportNightShift(reportData, reportFirstSheet);
+}
+
+function  fillReportSubHeader(reportData, reportFirstSheet) {
+	var	reportArriveTime = reportData.searchFieldResponse("Horário de chegada a obra");
+	var	reportExitTime = reportData.searchFieldResponse("Horário de saída da obra");
+	var	reportLunchTime = reportData.searchFieldResponse("Tempo total de intervalo de almoço");
+	var reportNumOfEmployees = reportData.searchFieldResponse("Quantidade de colaboradores (apenas turno diurno)");
+	
+	reportFirstSheet.getRange("B7").setValue(reportArriveTime);
+	reportFirstSheet.getRange("B8").setValue(reportExitTime);
+	reportFirstSheet.getRange("H7").setValue(reportLunchTime);
+	reportFirstSheet.getRange("L7").setValue(reportNumOfEmployees);
+}
+
+function fillReportHeader(reportData, reportFirstSheet) {
   reportFirstSheet.getRange("J5").setValue(reportData.rdo);
   reportFirstSheet.getRange("L5").setValue(reportData.date);
+  reportFirstSheet.getRange("B6").setValue(reportData.getClient());
+  reportFirstSheet.getRange("F6").setValue(reportData.getCNPJ());
+  reportFirstSheet.getRange("K6").setValue(reportData.getProposal());
 }
 
 function onFormSubmit(formData) {
@@ -106,7 +142,7 @@ function onFormSubmit(formData) {
   let reportData = new ReportData(formObject);
   reportData.reportSpreadSheetFile = createReportSpreadSheetFile(reportData);
   reportData.openReportSpreadSheet();
-  fillReportHeader(reportData);
+  fillReport(reportData);
 
   reportData.reportInfo.updateRDO(reportData.name);
   reportData.reportInfo.updateReportInfo();
