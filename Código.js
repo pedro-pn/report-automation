07a81070b806f4e892c68c85f8c6fe97e96f0b1d
Pedro Paulo <pe.pn03@gmail.com>
2024-05-15 18:16:48 -0300

    Add day shift overtime full calculation (including weekend)

--- Código.js
+++ Código.js
@@ -1,104 +1,110 @@
 const greportModelID = "1stkfMCGdpIDEc1u4xfS3Ldl9qXirgR_4mPYMcCjSxBM" 
 const reportInfoID = "1CEXqNgVBJOohszlvzw3B10nchUQ2eUIk"
 
-const weekday = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
+const weekDays = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
 
 const FormsFields = {
-  Date: "Data do relatório",
-  Project: "Projeto",
-  DayShiftStartTime: "Horário de chegada a obra",
-  DayShiftExitTime: "Horário de saída da obra",
-  TotalLunchTime: "Tempo total de intervalo de almoço",
-  DayShiftNumOfEmployees: "Quantidade de colaboradores (apenas turno diurno)",
-  NightShift: "Houve turno noturno?",
-  NightShiftStartTime: "Horário de inicio do turno noturno",
-  NightShiftEndTime: "Horário de saída do turno noturno",
-  TotalDinnerTime: "Tempo total de intervalo de janta",
-  NightShiftNumOfEmployees: "Quantidade de colaboradores no turno noturno",
+	Date: "Data do relatório",
+	Project: "Projeto",
+	DayShiftStartTime: "Horário de chegada a obra",
+	DayShiftExitTime: "Horário de saída da obra",
+	TotalLunchTime: "Tempo total de intervalo de almoço",
+	DayShiftNumOfEmployees: "Quantidade de colaboradores (apenas turno diurno)",
+	NightShift: "Houve turno noturno?",
+	NightShiftStartTime: "Horário de inicio do turno noturno",
+	NightShiftEndTime: "Horário de saída do turno noturno",
+	TotalDinnerTime: "Tempo total de intervalo de janta",
+	NightShiftNumOfEmployees: "Quantidade de colaboradores no turno noturno",
 }
 
 class ReportInfo {
 	constructor() {
-    	this.reportInfoFile = DriveApp.getFileById(reportInfoID);
-    	this.reportInfoString = this.reportInfoFile.getBlob().getDataAsString();
-    	this.reportInfoData = JSON.parse(this.reportInfoString);
-  	}
+			this.reportInfoFile = DriveApp.getFileById(reportInfoID);
+			this.reportInfoString = this.reportInfoFile.getBlob().getDataAsString();
+			this.reportInfoData = JSON.parse(this.reportInfoString);
+		}
 
-  // This function MUST be called after using reportInfo.json. 
-  	updateReportInfo() {
-   		const updatedInfoData = JSON.stringify(this.reportInfoData, null, 2);
-  		this.reportInfoFile.setContent(updatedInfoData);
+	// This function MUST be called after using reportInfo.json. 
+		updateReportInfo() {
+	 		const updatedInfoData = JSON.stringify(this.reportInfoData, null, 2);
+			this.reportInfoFile.setContent(updatedInfoData);
 	}
 
 	getProjectInfo(projectName) {
-    	return (this.reportInfoData.Projects.find(project => project.Name === projectName));
-  	}
+			return (this.reportInfoData.Projects.find(project => project.Name === projectName));
+		}
 
 	updateRDO(projectName) {
-    	this.getProjectInfo(projectName).RDO += 1;
-  	}
+			this.getProjectInfo(projectName).RDO += 1;
+		}
 }
 
 class ReportData {
-  constructor(formObject) {
-    this.formObject = formObject;
-    this.reportInfo = new ReportInfo();
-    this.name = this.getProjectName();
-    this.date = this.getReportDate();
-    this.rdo = this.getRDONumber() + 1;
-    this.reportSpreadSheet;
-    this.reportSpreadSheetFile;
-  }
+	constructor(formObject) {
+		this.formObject = formObject;
+		this.reportInfo = new ReportInfo();
+		this.name = this.getProjectName();
+		this.date = this.getReportDate();
+		this.rdo = this.getRDONumber() + 1;
+		this.reportSpreadSheet;
+		this.reportSpreadSheetFile;
+	}
 
-  getRDONumber() {
-    return (this.reportInfo.getProjectInfo(this.name).RDO);
-  }
+	getRDONumber() {
+		return (this.reportInfo.getProjectInfo(this.name).RDO);
+	}
 
-  getClient() {
-    return (this.reportInfo.getProjectInfo(this.name).Client);
-  }
+	getClient() {
+		return (this.reportInfo.getProjectInfo(this.name).Client);
+	}
 
-  getCNPJ() {
-    return (this.reportInfo.getProjectInfo(this.name).CNPJ);
-  }
+	getCNPJ() {
+		return (this.reportInfo.getProjectInfo(this.name).CNPJ);
+	}
 
-  getProposal() {
-    return (this.reportInfo.getProjectInfo(this.name).Proposal);
-  }
+	getProposal() {
+		return (this.reportInfo.getProjectInfo(this.name).Proposal);
+	}
 
-  getWeekDay() {
-    var dateStrings = this.date.split('-');
-    var dateType = new Date(dateStrings[2], dateStrings[1] - 1, dateStrings[0]);
-    var weekDay = dateType.getDay()
-    return (weekday[weekDay]);
-  }
-  
-  searchFieldResponse(fieldName) {
-      var itemResponses = this.formObject.getItemResponses();
-      for (var i = 0; i < itemResponses.length; i++) {
-        var itemResponse = itemResponses[i];
-        if (itemResponse.getItem().getTitle() == fieldName)
-          return (itemResponse.getResponse());
-      }
-    }
+	getWeekDayNum() {
+		var dateStrings = this.date.split('-');
+		var dateType = new Date(dateStrings[2], dateStrings[1] - 1, dateStrings[0]);
+		var weekDay = dateType.getDay();
+		
+		return (weekDay);
+	}
 
-  getReportDate() {
-      var date = this.searchFieldResponse(FormsFields.Date);
-      var dateComponents = date.split("-");
-      var day = dateComponents[2];
-      var month = dateComponents[1];
-      var year = dateComponents[0];
-    
-      return (day + '-' + month + '-' + year);
-    }
-    
-    getProjectName() {
-      return (this.searchFieldResponse(FormsFields.Project));
-    }
+	getWeekDay() {
+		var weekDay = this.getWeekDayNum();
+		return (weekDays[weekDay]);
+	}
+	
+	searchFieldResponse(fieldName) {
+			var itemResponses = this.formObject.getItemResponses();
+			for (var i = 0; i < itemResponses.length; i++) {
+				var itemResponse = itemResponses[i];
+				if (itemResponse.getItem().getTitle() == fieldName)
+					return (itemResponse.getResponse());
+			}
+		}
 
-    openReportSpreadSheet() {
-      this.reportSpreadSheet = SpreadsheetApp.open(this.reportSpreadSheetFile);
-    }
+	getReportDate() {
+			var date = this.searchFieldResponse(FormsFields.Date);
+			var dateComponents = date.split("-");
+			var day = dateComponents[2];
+			var month = dateComponents[1];
+			var year = dateComponents[0];
+		
+			return (day + '-' + month + '-' + year);
+		}
+		
+		getProjectName() {
+			return (this.searchFieldResponse(FormsFields.Project));
+		}
+
+		openReportSpreadSheet() {
+			this.reportSpreadSheet = SpreadsheetApp.open(this.reportSpreadSheetFile);
+		}
 }
 
 function	fillReportNightShift(reportData, reportFirstSheet) {
@@ -119,70 +125,98 @@ function	fillReportNightShift(reportData, reportFirstSheet) {
 
 
 function hourStringToDate(hourString) {
-  const [hours, minutes] = hourString.split(':');
-  const date = new Date();
-  date.setHours(parseInt(hours, 10));
-  date.setMinutes(parseInt(minutes, 10));
-  
-  return (date);
+	const [hours, minutes] = hourString.split(':');
+	const date = new Date();
+	date.setHours(parseInt(hours, 10));
+	date.setMinutes(parseInt(minutes, 10));
+	
+	return (date);
 }
 
+/**
+ * Calculates the time difference from the given time stamp string. The string must be in the
+ * "XX:XX" format.
+ *
+ * @param {string} startHourString
+ * @param {string} endHourString
+ * @return {number} 
+ */
 function getDiffHour(startHourString, endHourString) {
-    const startTime = hourStringToDate(startHourString);
-    const endTime = hourStringToDate(endHourString);
-    const timeDifference = endTime.getTime() - startTime.getTime();
-    const hourDifference = timeDifference / (1000 * 60 * 60);
+	const startTime = hourStringToDate(startHourString);
+	const endTime = hourStringToDate(endHourString);
+	const timeDifference = endTime.getTime() - startTime.getTime();
+	const hourDifference = timeDifference / (1000 * 60 * 60);
 
-    return Math.abs(hourDifference);
+	return (hourDifference);
 }
 
+/**
+ * Converts a time stamp number to a time stamp string in the format "XX:XX".
+ *
+ * @param {number} hours
+ * @return {string} 
+ */
 function hoursToHourString(hours) {
-    const wholeHours = Math.floor(hours);
-    const minutes = Math.round((hours - wholeHours) * 60);
+	const wholeHours = Math.floor(hours);
+	const minutes = Math.round((hours - wholeHours) * 60);
 
-    const paddedHours = wholeHours < 10 ? '0' + wholeHours : wholeHours;
-    const paddedMinutes = minutes < 10 ? '0' + minutes : minutes;
+	const paddedHours = wholeHours < 10 ? '0' + wholeHours : wholeHours;
+	const paddedMinutes = minutes < 10 ? '0' + minutes : minutes;
 
-    const hourString = `${paddedHours}:${paddedMinutes}`;
+	const hourString = `${paddedHours}:${paddedMinutes}`;
 
-    return (hourString);
+	return (hourString);
 }
 
-function isNotOvertime(overtime) {
-    console.log(overtime);
-    if (overtime <= 10.50)
-        return (true);
-    return (false)
+function getShiftTime(reportData) {
+	const weekday = reportData.getWeekDayNum();
+	const saturdayFlag = reportData.reportInfo.getProjectInfo(reportData.name).IncludeSaturday;
+	if (weekday > 0 && weekday < 5)
+		return ("09:00");
+	if (weekday == 5)
+		return ("08:00");
+	if (weekday == 6 && saturdayFlag)
+		return ("08:00");
+	return ("00:00");
+}
+
+function calculateOvertime(shiftTime, totalShiftTime) {
+	const overtime = getDiffHour(shiftTime, hoursToHourString(totalShiftTime));
+
+	return (overtime);
+}
+
+function calculateDayShiftTime(reportData) {
+	const dayShiftStartTime = reportData.searchFieldResponse(FormsFields.DayShiftStartTime);
+	const dayShiftExitTime = reportData.searchFieldResponse(FormsFields.DayShiftExitTime);
+	const lunchInterval = reportData.searchFieldResponse(FormsFields.TotalLunchTime);
+	const totalShiftTime = getDiffHour(dayShiftStartTime, dayShiftExitTime);
+	const shiftTime = getDiffHour(lunchInterval, hoursToHourString(totalShiftTime));
+
+	return (shiftTime);
 }
 
 function fillDayShiftOvertime(reportData, reportFirstSheet) {
-    const dayShiftStartTime = reportData.searchFieldResponse(FormsFields.DayShiftStartTime);
-    const dayShiftExitTime = reportData.searchFieldResponse(FormsFields.DayShiftExitTime);
-    const totalShiftTime = getDiffHour(dayShiftStartTime, dayShiftExitTime)
-
-    if (isNotOvertime(totalShiftTime))
-        return ;
-
-    const lunchInterval = reportData.searchFieldResponse(FormsFields.TotalLunchTime);
-    const rawOvertime = getDiffHour(hoursToHourString(totalShiftTime), "09:00");
-    console.log(lunchInterval);
-    console.log(typeof lunchInterval);
-    const overtime = getDiffHour(hoursToHourString(rawOvertime), lunchInterval);
-    reportFirstSheet.getRange("D63").setValue(hoursToHourString(overtime));
+	const dayShiftTime = calculateDayShiftTime(reportData);
+	const shiftTime = getShiftTime(reportData);
+	const overtime = calculateOvertime(shiftTime, dayShiftTime);
+	if (overtime <= 0.5)
+			return ;
+	reportFirstSheet.getRange("D63").setValue(hoursToHourString(overtime));
 }
 
 function fillReportFooter(reportData, reportFirstSheet) {
-    fillDayShiftOvertime(reportData, reportFirstSheet)
+		fillDayShiftOvertime(reportData, reportFirstSheet)
 }
 
 function  fillReport(reportData) {
-  var reportSpreadSheet = reportData.reportSpreadSheet;
-  var reportFirstSheet = reportSpreadSheet.getSheets()[0];
+	var reportSpreadSheet = reportData.reportSpreadSheet;
+	var reportFirstSheet = reportSpreadSheet.getSheets()[0];
 
-  fillReportHeader(reportData, reportFirstSheet);
-  fillReportSubHeader(reportData, reportFirstSheet);
-  fillReportNightShift(reportData, reportFirstSheet);
-  fillReportFooter(reportData, reportFirstSheet);
+	fillReportHeader(reportData, reportFirstSheet);
+	fillReportSubHeader(reportData, reportFirstSheet);
+	fillReportNightShift(reportData, reportFirstSheet);
+	fillReportFooter(reportData, reportFirstSheet);
 }
 
 function  fillReportSubHeader(reportData, reportFirstSheet) {
@@ -198,48 +232,48 @@ function  fillReportSubHeader(reportData, reportFirstSheet) {
 }
 
 function fillReportHeader(reportData, reportFirstSheet) {
-  reportFirstSheet.getRange("J5").setValue(reportData.rdo);
-  reportFirstSheet.getRange("L5").setValue(reportData.date);
-  reportFirstSheet.getRange("B6").setValue(reportData.getClient());
-  reportFirstSheet.getRange("F6").setValue(reportData.getCNPJ());
-  reportFirstSheet.getRange("K6").setValue(reportData.getProposal());
+	reportFirstSheet.getRange("J5").setValue(reportData.rdo);
+	reportFirstSheet.getRange("L5").setValue(reportData.date);
+	reportFirstSheet.getRange("B6").setValue(reportData.getClient());
+	reportFirstSheet.getRange("F6").setValue(reportData.getCNPJ());
+	reportFirstSheet.getRange("K6").setValue(reportData.getProposal());
 }
 
 function onFormSubmit(formData) {
-  var formObject = formData.response
-  let reportData = new ReportData(formObject);
-  reportData.reportSpreadSheetFile = createReportSpreadSheetFile(reportData);
-  reportData.openReportSpreadSheet();
-  fillReport(reportData);
+	var formObject = formData.response
+	let reportData = new ReportData(formObject);
+	reportData.reportSpreadSheetFile = createReportSpreadSheetFile(reportData);
+	reportData.openReportSpreadSheet();
+	fillReport(reportData);
 
-  reportData.reportInfo.updateRDO(reportData.name);
-  reportData.reportInfo.updateReportInfo();
+	reportData.reportInfo.updateRDO(reportData.name);
+	reportData.reportInfo.updateReportInfo();
 
 }
 
 function createReportSpreadSheetFile(reportData) {
-  var modelSpreadSheetFile = SpreadsheetApp.openById(greportModelID);
-  var copierSpreadSheetFile = DriveApp.getFileById(modelSpreadSheetFile.getId()).makeCopy();
-  copierSpreadSheetFile.setName(reportData.name + ' - RDO ' + reportData.rdo + ' - ' + reportData.date + ' - ' + reportData.getWeekDay());
-  return (copierSpreadSheetFile);
+	var modelSpreadSheetFile = SpreadsheetApp.openById(greportModelID);
+	var copierSpreadSheetFile = DriveApp.getFileById(modelSpreadSheetFile.getId()).makeCopy();
+	copierSpreadSheetFile.setName(reportData.name + ' - RDO ' + reportData.rdo + ' - ' + reportData.date + ' - ' + reportData.getWeekDay());
+	return (copierSpreadSheetFile);
 }
 
 function moveFile() {
-  // Replace 'FILE_ID' with the ID of the file you want to move
-  var fileId = 'FILE_ID';
-  
-  // Replace 'FOLDER_ID' with the ID of the folder where you want to move the file
-  var folderId = 'FOLDER_ID';
-  
-  // Get the file
-  var file = DriveApp.getFileById(fileId);
-  
-  // Get the destination folder
-  var folder = DriveApp.getFolderById(folderId);
-  
-  // Move the file to the destination folder
-  folder.addFile(file);
-  
-  // Remove the file from its current location (if needed)
-  DriveApp.getRootFolder().removeFile(file);
+	// Replace 'FILE_ID' with the ID of the file you want to move
+	var fileId = 'FILE_ID';
+	
+	// Replace 'FOLDER_ID' with the ID of the folder where you want to move the file
+	var folderId = 'FOLDER_ID';
+	
+	// Get the file
+	var file = DriveApp.getFileById(fileId);
+	
+	// Get the destination folder
+	var folder = DriveApp.getFolderById(folderId);
+	
+	// Move the file to the destination folder
+	folder.addFile(file);
+	
+	// Remove the file from its current location (if needed)
+	DriveApp.getRootFolder().removeFile(file);
 }
